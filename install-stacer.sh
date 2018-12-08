039144235bb2b15846eccb8c16098d07a6b9a6b0
Emanuele Ballarin <emanuele@ballarin.cc>
2018-12-08 02:58:13 +0100

    Added install script

---
+++ install-stacer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SELF_CALLDIR="$(pwd)"
+SELF_BUILDIR="$SELF_CALLDIR/staged"
+
+rm -R -f ./staged
+mkdir -p "$SELF_BUILDIR"
+cd "$SELF_BUILDIR"
+
+git clone --recursive --depth=1 https://github.com/emaballarin/Stacer.git
+
+cd Stacer
+mkdir build
+cd build
+
+cmake .. -DCMAKE_BUILD_TYPE="Release"
+make -j8
+
+sudo make install
+sudo make install
+
+cd "$SELF_CALLDIR"
+rm -R -f ./staged
